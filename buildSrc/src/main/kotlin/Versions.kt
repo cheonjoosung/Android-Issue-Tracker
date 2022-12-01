de8f9d24fa6abc0523bf1278e3cfbe476779963e
cheonjoosung <cjswntjd@gmail.com>
2022-12-01 14:24:50 +0900

    buildSRC 의존성 별도관리 파일 생성

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,14 @@
+object AndroidX {
+
+    const val CORE_KTX = "androidx.core:core-ktx:1.9.0"
+    const val APPCOMPAT = "androidx.appcompat:appcompat:1.5.1"
+    const val CONSTRAINTLayout = "androidx.constraintlayout:constraintlayout:2.1.4"
+    const val LIFECYCLE_LIVEDATA_KTX = "androidx.lifecycle:lifecycle-livedata-ktx:2.5.1"
+    const val LIFECYCLE_VIEWMODEL_KTX = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1"
+    const val NAVIGATION_FRAGMENT_KTX = "androidx.navigation:navigation-fragment-ktx:2.5.3"
+    const val NAVIGATION_UI_KTX = "androidx.navigation:navigation-ui-ktx:2.5.3"
+}
+
+object Google {
+    const val MATERIAL = "com.google.android.material:material:1.7.0"
+}
